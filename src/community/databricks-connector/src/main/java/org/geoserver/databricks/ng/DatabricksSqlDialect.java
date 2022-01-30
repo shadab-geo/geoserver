1854ffd469485473400e919a523f039b3c70f9d6
Nuno Oliveira <nuno.oliveira@geo-solutions.it>
2022-01-30 23:48:29 +0000

    Add first version of databricks connector

---
+++ src/community/databricks-connector/src/main/java/org/geoserver/databricks/ng/DatabricksSqlDialect.java
@@ -0,0 +1,54 @@
+package org.geoserver.databricks.ng;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.geotools.jdbc.BasicSQLDialect;
+import org.geotools.jdbc.JDBCDataStore;
+import org.geotools.util.factory.Hints;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.opengis.feature.type.GeometryDescriptor;
+
+public final class DatabricksSqlDialect extends BasicSQLDialect {
+
+    protected DatabricksSqlDialect(JDBCDataStore dataStore) {
+        super(dataStore);
+    }
+
+    @Override
+    public void encodeGeometryValue(Geometry value, int dimension, int srid, StringBuffer sql)
+            throws IOException {
+        throw new RuntimeException("Spatial geometries are not supported yet!");
+    }
+
+    @Override
+    public void encodeGeometryEnvelope(String tableName, String geometryColumn, StringBuffer sql) {
+        throw new RuntimeException("Spatial geometries are not supported yet!");
+    }
+
+    @Override
+    public Envelope decodeGeometryEnvelope(ResultSet rs, int column, Connection cx)
+            throws SQLException, IOException {
+        throw new RuntimeException("Spatial geometries are not supported yet!");
+    }
+
+    @Override
+    public Geometry decodeGeometryValue(
+            GeometryDescriptor descriptor,
+            ResultSet rs,
+            String column,
+            GeometryFactory factory,
+            Connection cx,
+            Hints hints)
+            throws IOException, SQLException {
+        throw new RuntimeException("Spatial geometries are not supported yet!");
+    }
+
+    @Override
+    public boolean isAutoCommitQuery() {
+        return true;
+    }
+}
